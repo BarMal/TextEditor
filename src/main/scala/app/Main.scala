9804bdabd80dc543641ddd871c12bbb51a30bc3b
BarMal <barnabymalaj@gmail.com>
2025-03-23 00:02:47 +0000

    Mess trying to get multiple streams working in parallel

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -3,12 +3,18 @@ package app
 import app.config.AppConfig
 import app.config.AppConfig.yamlDecoder
 import app.terminal.Term
-import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.effect.{ExitCode, IO, IOApp, Ref, Resource}
+import fs2.Chunk
+import fs2.Chunk.Queue
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 import org.virtuslab.yaml.*
 
 import scala.language.postfixOps
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+import cats.syntax.parallel.catsSyntaxTuple2Parallel
+import cats.effect.implicits.parallelForGenSpawn
 
 object Main extends IOApp {
 
@@ -21,19 +27,116 @@ object Main extends IOApp {
   private def terminal: Resource[IO, Term[IO]] =
     Resource.make(Term.createF[IO])(_.close)
 
+  private val stateRef: IO[Ref[IO, BufferState]] =
+    Ref.of[IO, BufferState](BufferState.empty)
+
+  private val cursorBlinkInterval: FiniteDuration = 500 milliseconds
+
+  private def in(term: Term[IO]): fs2.Stream[IO, BufferState] =
+    term.readStream
+      .scan(BufferState.empty)(_ ++ _)
+      .evalTap(state => stateRef.map(ref => ref.update(_ => state)))
+
+  private def render(term: Term[IO]) =
+    fs2.Stream
+      .constant(System.currentTimeMillis())
+      .evalTap { startTime =>
+        val elapsedTime = System.currentTimeMillis() - startTime
+        val shouldBlink: Boolean = Math.floorDiv(
+          elapsedTime,
+          cursorBlinkInterval.toMillis
+        ) % 2 == 0
+
+        logger.info(s"""Rendering with start $startTime, elapsed $elapsedTime""")
+
+        for {
+          ref   <- stateRef
+          state <- ref.get
+          _ <- term.print(
+            state,
+            (
+              Math.floorMod(state.cursorPosition, state.lineLength),
+              Math.floorDiv(state.cursorPosition, state.lineLength)
+            ),
+            shouldBlink
+          )
+        } yield ()
+      }
+
   private def processStream: Term[IO] => IO[ExitCode] = term =>
     term.readStream
       .scan(BufferState.empty)(_ ++ _)
-//      .takeThrough(_.exitCondition)
-//      .evalTap(term.print)
-      .through(state => term._print(state, state.cursorPosition))
+      .parEvalMap(1) { state =>
+        term.print(
+          state,
+          (
+            Math.floorMod(state.cursorPosition, state.lineLength),
+            Math.floorDiv(state.cursorPosition, state.lineLength)
+          ),
+          false
+        )
+      }
       .compile
       .drain
       .as(ExitCode.Success)
+//      .evalTap { state =>
+//        print(
+//          state,
+//          (
+//            Math.floorMod(state.cursorPosition, state.lineLength),
+//            Math.floorDiv(state.cursorPosition, state.lineLength)
+//          ),
+//          Math.floorDiv(
+//            System.currentTimeMillis() - startTime,
+//            cursorBlinkInterval.toMillis
+//          ) % 2 == 0
+//        )
+//      }
+////      .takeThrough(_.exitCondition)
+//      .compile
+//      .drain
+//      .as(ExitCode.Success)
+
+  // Shared state: BufferState (text editor contents) + cursor visibility
+//  Ref.of[IO, (BufferState, Boolean)]((BufferState.empty, false)).flatMap { stateRef =>
+//
+//    // Input Stream: Updates BufferState in Ref
+//    val inputStream: Term[IO] => fs2.Stream[IO, BufferState] = term => term.readStream
+//      .scan(BufferState.empty)(_ ++ _)
+//      .evalTap(newState => stateRef.update { case (_, cursorVisible) => (newState, cursorVisible) })
+//
+//    // Cursor Blink Stream: Toggles cursor visibility in Ref every 500ms
+//    val cursorBlinkStream = fs2.Stream
+//      .awakeEvery[IO](500.millis)
+//      .mapAccumulate(false)((visible, _) => (!visible, !visible))
+//      .map(_._2)
+//      .evalMap(cursorVisible => stateRef.update { case (state, _) => (state, cursorVisible) })
+//
+//    // Render Stream: Periodically reads Ref & updates terminal
+//    val renderStream: Term[IO] => fs2.Stream[IO, (BufferState, Boolean)] = term => fs2.Stream
+//      .awakeEvery[IO](16.millis) // ~60FPS rendering
+//      .evalMap(_ => stateRef.get)
+//      .evalTap { case (state, cursorVisible) =>
+//        term.print(
+//          state,
+//          (
+//            Math.floorMod(state.cursorPosition, state.lineLength),
+//            Math.floorDiv(state.cursorPosition, state.lineLength)
+//          ),
+//          cursorVisible
+//        )
+//      }
+//
+//    // Run all streams concurrently
+//    (inputStream, cursorBlinkStream).parTupled *> renderStream.as(ExitCode.Success)
+//  }
+
+  private def newProcess: Term[IO] => IO[ExitCode] = term =>
+    render(term).evalTap(_ => in(term).compile.drain).compile.drain.as(ExitCode.Success)
 
   def run(args: List[String]): IO[ExitCode] =
     terminal
-      .use(processStream)
+      .use(newProcess)
       .handleErrorWith(error =>
         logger
           .error(error)("Something went wrong")
--- src/main/scala/app/terminal/Term.scala
+++ src/main/scala/app/terminal/Term.scala
@@ -1,59 +1,51 @@
 package app.terminal
 
+import app.BufferState
 import cats.effect.kernel.Async
 import cats.implicits.toFunctorOps
 import cats.{Applicative, Show}
-import com.googlecode.lanterna.{input, TerminalSize}
 import com.googlecode.lanterna.input.KeyStroke
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration.BoldMode
 import com.googlecode.lanterna.terminal.{DefaultTerminalFactory, Terminal}
+import com.googlecode.lanterna.{TerminalSize, input}
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jFactory
-import scala.concurrent.duration.DurationInt
 
+import java.awt.Font
+import scala.concurrent.duration.FiniteDuration
 import scala.language.postfixOps
 
-class Term[F[_]: Async](terminal: Terminal) {
+class Term[F[_]: Async](
+    terminal: Terminal
+) {
 
   private val logger: SelfAwareStructuredLogger[F] =
     Slf4jFactory.create[F].getLogger
 
-  private val fTerm = Async[F].blocking(terminal)
+  private lazy val fTerm: F[Terminal] = Async[F].blocking(terminal)
 
-  private val cursorTimerStream: fs2.Stream[F, Boolean] =
-    fs2.Stream.awakeEvery(500 milliseconds).evalScan(false) {
-      case (visible, _) => Applicative[F].pure(!visible)
-    }
+//  def renderStream(in: fs2.Stream[F, BufferState]): fs2.Stream[F, Unit] =
+//    in.evalTap { state =>
+//      print(
+//        state,
+//        (
+//          Math.floorMod(state.cursorPosition, state.lineLength),
+//          Math.floorDiv(state.cursorPosition, state.lineLength)
+//        ),
+//        Math.floorDiv(System.currentTimeMillis() - startTime, cursorBlinkInterval.toMillis) % 2 == 0
+//      )
+//    }.void
 
-  def _print[T: Show](t: T, cursorPos: Int): fs2.Stream[F, Boolean] =
-    cursorTimerStream.evalTap { visible =>
-      fTerm.map { term =>
-        term.clearScreen()
-        term.setCursorVisible(visible)
-        Show[T].show(t).foreach { char =>
-          try term.putCharacter(char)
-          catch
-            case ex =>
-              logger.error(ex)(s"""Error printing character for $t""")
-          term.putCharacter
-        }
-        term.flush()
-      }
-    }
-  //    fTerm.map { term =>
-//      term.clearScreen()
-//      Show[T].show(t).foreach { char =>
-//        try term.putCharacter(char)
-//        catch
-//          case ex =>
-//            logger.error(ex)(s"""Error printing character for $t""")
-//        term.putCharacter
-//      }
-//      term.flush()
-//    }
-
-  def print[T: Show](t: T): F[Unit] =
+  def print[T: Show](
+      t: T,
+      cursorPosition: (Int, Int),
+      cursorVisibility: Boolean
+  ): F[Unit] =
     fTerm.map { term =>
       term.clearScreen()
+      term.setCursorVisible(cursorVisibility)
+      term.setCursorPosition(cursorPosition._1, cursorPosition._2)
       Show[T].show(t).foreach { char =>
         try term.putCharacter(char)
         catch
@@ -67,17 +59,18 @@ class Term[F[_]: Async](terminal: Terminal) {
   def readStream: fs2.Stream[F, KeyStroke] =
     fs2.Stream.repeatEval(fTerm.map(_.readInput()))
 
-  def close: F[Unit] = Async[F].blocking(terminal.close())
+  def close: F[Unit] = fTerm.map(_.close())
 
 }
 
 object Term {
-  def createF[F[_]: Async]: F[Term[F]] = Applicative[F].pure {
-    val term = DefaultTerminalFactory()
-      .setTerminalEmulatorTitle("")
-      .setInitialTerminalSize(new TerminalSize(75, 50))
-      .createTerminal()
-    term.enterPrivateMode()
-    new Term(term)
-  }
+  def createF[F[_]: Async]: F[Term[F]] =
+    Applicative[F].pure {
+      val term = DefaultTerminalFactory()
+        .setTerminalEmulatorTitle("")
+        .setInitialTerminalSize(new TerminalSize(75, 50))
+        .createTerminal()
+      term.enterPrivateMode()
+      new Term(term)
+    }
 }
